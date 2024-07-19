0c272d7ac88bae99e9e7a84aa36850504e679b5c
otles2 <otles2@wisc.edu>
2024-07-19 16:33:55 -0500

    copied fulll solution

--- backend/api/Counter.cs
+++ backend/api/Counter.cs
@@ -1,14 +1,13 @@
 using Newtonsoft.Json;
+
 namespace Company.Function
 {
-    public class Counter
+    public class Counter 
     {
-        [JsonProperty(PropertyName ="id")]
-        public string Id { get; set; }
-        [JsonProperty(PropertyName ="count")]
-        public int Count { get; set; }
+        [JsonProperty(PropertyName="id")]
+        public string Id {get; set;}
+        
+        [JsonProperty(PropertyName = "count")]
+        public int Count {get;set;}
     }
-}   
- 
- The Counter class is a simple class that has a single property, Value, which is an integer. This class is used to store the value of the counter. 
- The Counter class is used in the following code snippet:
+}
--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -7,8 +7,6 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Data.Common;
-using System.Runtime.CompilerServices;
 using System.Net.Http;
 using System.Text;
 
@@ -19,20 +17,24 @@ namespace Company.Function
         [FunctionName("GetResumeCounter")]
         public static HttpResponseMessage Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            [CosmosDB(databaseName:"doitcosmosdb",collectionName:"Counter",ConnectionStringSetting = "AzureCiCdConnectionString"),Id="1",Partitionkey="1")] Counter counter,
-            [CosmosDB(databaseName:"doitcosmosdb",collectionName:"Counter",ConnectionStringSetting = "AzureCiCdConnectionString"),Id="1",PartitionKey="1")] out Counter updatedcounter,
-
+            [CosmosDB(databaseName:"AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
+            [CosmosDB(databaseName:"AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter,
             ILogger log)
         {
+            // Here is where the counter gets updated.
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            updatedcounter = counter;
-            updatedcounter.Count += 1;  
-            var jsonToReturn = JsonConvert.SerializeObject(counter);
-            return  new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            updatedCounter = counter;
+            updatedCounter.Count += 1;
+
+            var jsonToRetun = JsonConvert.SerializeObject(counter);
+
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
             {
-                Content = new StringContent(jsonToReturn,Endcoding.UTF8,"application/json")
+                Content = new StringContent(jsonToRetun, Encoding.UTF8, "application/json")
             };
+
+            
         }
     }
 }
